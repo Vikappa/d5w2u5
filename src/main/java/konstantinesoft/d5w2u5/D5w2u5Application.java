d207b2d7b433f7efd9c1537094fa79c26c43b50d
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-04-12 10:16:40 +0200

    Project created

---
+++ src/main/java/konstantinesoft/d5w2u5/D5w2u5Application.java
@@ -0,0 +1,13 @@
+package konstantinesoft.d5w2u5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D5w2u5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D5w2u5Application.class, args);
+	}
+
+}
---
+++ src/test/java/konstantinesoft/d5w2u5/D5w2u5ApplicationTests.java
@@ -0,0 +1,13 @@
+package konstantinesoft.d5w2u5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D5w2u5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
